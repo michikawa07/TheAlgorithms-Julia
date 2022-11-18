4413198b84bd28a8ac74bb75c9ab9cea7ea43d91
michikawa07 <michikawa.ryohei@gmail.com>
2022-11-18 13:17:35 +0900

    fix format

--- src/math/two_sum.jl
+++ src/math/two_sum.jl
@@ -1,8 +1,7 @@
-function two_sum(nums, target::Integer)
-	for (i, first) in enumerate(nums)
-		diff = target - first
-		diff ∈ nums && return (i, findlast(==(diff),nums))
-	end
-	return ()
+function two_sum(nums::AbstractVector{<:Integer}, target::Integer)
+    for (i, first) in enumerate(nums)
+        diff = target - first
+        diff ∈ nums && return (i, findlast(==(diff), nums))
+    end
+    return ()
 end
-
