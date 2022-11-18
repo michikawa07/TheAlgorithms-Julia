509067223dc46412868d6ce78244e31b232eaa27
michikawa07 <michikawa.ryohei@gmail.com>
2022-11-18 14:20:03 +0900

    add doc

--- src/math/two_sum.jl
+++ src/math/two_sum.jl
@@ -1,3 +1,25 @@
+"""
+	two_sum(nums::AbstractVector{<:Integer}, target::Integer)
+
+Given an array of integers, return indices of the two numbers such that they add up to
+a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the
+same element twice.
+
+# Example:
+
+```
+nums = [2, 7, 11, 15]
+target = 9
+
+two_sum(nums, target)
+
+# Output, because nums[1] + nums[2] = 2 + 7 = 9
+(1, 2) 
+```
+
+"""
 function two_sum(nums::AbstractVector{<:Integer}, target::Integer)
     for (i, first) in enumerate(nums)
         diff = target - first
